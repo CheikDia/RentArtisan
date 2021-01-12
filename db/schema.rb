70c8a6a075e3d866b273e16a12787bb32d7b2823
Rindra Randrianasolo <rindra.randrianasolo@gmail.com>
2021-01-12 21:54:36 +0100

    Solve database error

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_01_12_195605) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "bookings", force: :cascade do |t|
     t.date "start_date"
@@ -40,9 +37,6 @@ ActiveRecord::Schema.define(version: 2021_01_12_195605) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.string "region"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -53,7 +47,7 @@ ActiveRecord::Schema.define(version: 2021_01_12_195605) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-  
+
   add_foreign_key "bookings", "skills"
   add_foreign_key "bookings", "users"
   add_foreign_key "skills", "users"
