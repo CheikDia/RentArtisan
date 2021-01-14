796532c93d2d37032bd4747b75efb0620e57ed03
sebastien scirpo <sebastien.scirpo@gmail.com>
2021-01-14 20:15:07 +0100

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 100 fake users...'
+
+User.destroy_all
+
+100.times do
+  user = User.new(
+    name:    Faker::FunnyName.name,
+    description: Faker::Lorem.paragraph,
+    region: Faker::Address.state,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 6)
+      )
+  user.save!
+end
+puts 'Finished!'
