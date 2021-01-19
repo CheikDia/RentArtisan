9f6dcc6980197182e2b9d39835b56d877a4e949e
sebastien scirpo <sebastien.scirpo@gmail.com>
2021-01-19 09:38:00 +0100

    adding search bar

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,4 +2,10 @@ class Skill < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   has_many :bookings
+  include PgSearch::Model
+  pg_search_scope :search_skill,
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
