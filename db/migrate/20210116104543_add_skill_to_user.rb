ed2b6a59f81bc557ff756c604bf6d6d734f542f3
Cheikhou Diakhaby <diakhaby_cheik@yahoo.fr>
2021-01-21 23:17:39 +0100

    one migration file commented

--- db/migrate/20210116104543_add_skill_to_user.rb
+++ db/migrate/20210116104543_add_skill_to_user.rb
@@ -1,5 +1,5 @@
 class AddSkillToUser < ActiveRecord::Migration[6.0]
   def change
-    add_reference :users, :skill, foreign_key:true
+    #add_reference :users, :skill, foreign_key:true
   end
 end
